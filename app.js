6fa47cac62433ce25fbd81e5943fb12a2af5bf1e
falk <falk.altrock@imbus.de>
2020-09-17 08:13:18 +0200

    formatted

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // select h2 and button
-const h2 = document.getElementById("pin")
-const pinBtn = document.getElementById("pinbutton")
-const digitDisplay = document.getElementById("digitDisplay")
-const moreDigitsBtn = document.getElementById("moreDigits")
-const lessDigitsBtn = document.getElementById("lessDigits")
+const h2 = document.getElementById("pin");
+const pinBtn = document.getElementById("pinbutton");
+const digitDisplay = document.getElementById("digitDisplay");
+const moreDigitsBtn = document.getElementById("moreDigits");
+const lessDigitsBtn = document.getElementById("lessDigits");
 
 let digits = 4;
 
 // add event listeners
 pinBtn.addEventListener("click", () => {
-    h2.textContent = createPin();
-})
+  h2.textContent = createPin();
+});
 
 // increase number of digits
 moreDigitsBtn.addEventListener("click", () => {
-    digitDisplay.textContent = ++digits;
-})
+  digitDisplay.textContent = ++digits;
+});
 
 // decrease number of digits
 lessDigitsBtn.addEventListener("click", () => {
-    digits--
+  digits--;
 
-    if (digits<1) digits = 1; 
+  if (digits < 1) digits = 1;
 
-    digitDisplay.textContent = digits;
-})
+  digitDisplay.textContent = digits;
+});
 
 //create pin
 function createPin() {
-    let newPin = "";
+  let newPin = "";
 
-    for (let i = 0; i < digits; i++) {
-        newPin += Math.floor(Math.random() * 10)
-    }
+  for (let i = 0; i < digits; i++) {
+    newPin += Math.floor(Math.random() * 10);
+  }
 
-    return newPin
+  return newPin;
 }
